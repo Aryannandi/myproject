b983a15cb6a7af85999cdbc2cbae9e0c50b033ec
Aryannandi <aryannandi73@gmail.com>
2025-03-21 18:34:04 +0530

    added few features

---
+++ store/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from category.models import category
+
+# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    images = models.ImageField(upload_to='photos/product')
+    stock = models.IntegerField()
+    is_available = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.product_name
